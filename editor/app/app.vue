94b6a4dd85ac8027c274e004e44e45f7f48d6959
Mateusz Kulpa <mateusz.kulpa@outlook.com>
2025-09-07 00:21:14 +0200

    chore: setup editor app

---
+++ editor/app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <UApp>
+    <UHeader title="LiveKit Flows Editor" />
+
+    <UMain>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </UMain>
+  </UApp>
+</template>
---
+++ editor/app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <ClientOnly>
+    <div class="h-screen w-screen">
+      <FlowEditor />
+    </div>
+  </ClientOnly>
+</template>
